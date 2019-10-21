6f186e1f7b4f234f5caca4672e8274a43889398c
Aleksa Trampa <alektramp@gmail.com>
2019-10-21 19:01:01 +0200

    Typewritter superspeed trigger,  max rows

---
+++ Assets/Scripts/CodesQueue.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodesQueue 
+{
+	protected string[] codesArray = {
+			"i++",
+			"Hello World!",
+			"return 0;",
+			"i++",
+			"Hello World!",
+			"return 0;",
+			"i++",
+			"Hello World!",
+			"return 0;",
+			"i++",
+			"Hello World!",
+			"return 0;",
+			"i++",
+			"Hello World!",
+			"return 0;",
+			"i++",
+			"Hello World!",
+			"return 1;"
+			};
+	protected string[] temporaryCode = {
+		"answer 1;",
+		"answer2;",
+		"answer3;"
+	};
+
+	public string[] CodesArray { get => codesArray; set => codesArray = value; }
+
+	public CodesQueue()
+	{
+		
+	}
+}
--- Assets/Scripts/Typewritter.cs
+++ Assets/Scripts/Typewritter.cs
@@ -7,24 +7,52 @@ public class Typewritter : MonoBehaviour
 {
 
     Text txt;
-    string story;
+    string[] story;
+	public float typeSpeed = 0.075f;
+
 
     void Awake()
     {
+		CodesQueue codesQueue= new CodesQueue();
         txt = GetComponent<Text>();
-        story = txt.text;
+        //story = txt.text;
+		story = codesQueue.CodesArray;
         txt.text = "";
 
         // TODO: add optional delay when to start
         StartCoroutine("PlayText");
     }
 
+	void superSpeedTrigger()
+	{
+		typeSpeed = 0.002f;
+	}
+
     IEnumerator PlayText()
     {
-        foreach (char c in story)
-        {
-            txt.text += c;
-            yield return new WaitForSeconds(0.125f);
-        }
+		int i = 0;
+		int lineNumber = 0;
+		foreach(string line in story)
+		{
+			if (lineNumber % 3 == 0)
+			{
+				txt.text = "";
+			}
+			lineNumber++;
+			foreach (char c in line)
+			{
+				i++;
+
+				if (i == 40)
+				{
+					superSpeedTrigger();
+				}
+				txt.text += c;
+				yield return new WaitForSeconds(typeSpeed);
+			}
+			txt.text += "\n";
+			yield return new WaitForSeconds(0.002f);
+		}
+      
     }
 }
