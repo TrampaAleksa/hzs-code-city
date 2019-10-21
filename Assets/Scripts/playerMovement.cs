0e188bfee34eeafaf44cda60c0858315517e3a62
Paun <marko.paunovic1998@gmail.com>
2019-10-21 15:33:46 +0200

    Scripts folder
    Skripte ljudi :)

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class playerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
