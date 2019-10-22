310d87c870229fab50410c65663962ad7e3e7bf8
Aleksa Trampa <alektramp@gmail.com>
2019-10-22 14:08:57 +0200

    drag and drop in all directions with no logic

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begun dragging");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("dragging");
+
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("Ended dragging");
+    }
+}
