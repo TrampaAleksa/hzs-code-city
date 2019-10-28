05827a7c43b85a155b4a2f6c21f6083dc5c23bbc
Aleksa Trampa <alektramp@gmail.com>
2019-10-28 16:12:35 +0100

    Animation controller with keyboard - no buttons

---
+++ Assets/Pavle/Jana 1/CharacterAnim.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnim : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetTrigger("jump");
+        }
+
+    }
+}
