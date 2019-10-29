d2940a5876cd7ce7a9e1e38e1221628617f48270
Aleksa Trampa <alektramp@gmail.com>
2019-10-29 20:03:39 +0100

    main menu animation on text

---
+++ Assets/Scripts/MainMenuTypewritter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuTypewritter : MonoBehaviour
+{
+
+	Text txt;
+	string story;
+	public float typeSpeed;
+	public Image logo;
+	// Start is called before the first frame update
+	void Start()
+	{
+		txt = GetComponentInChildren<Text>();
+		story = txt.text;
+		txt.text = "";
+		StartCoroutine("PlayText");
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	IEnumerator PlayText()
+	{
+		
+			foreach (var c in story)
+			{
+				txt.text += c;
+				yield return new WaitForSeconds(typeSpeed);
+			}
+		logo.gameObject.SetActive(true);
+	}
+}
