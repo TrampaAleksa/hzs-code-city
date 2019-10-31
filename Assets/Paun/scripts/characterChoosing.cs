948ba36345b185760450c16104c82121776314b2
Aleksa Trampa <alektramp@gmail.com>
2019-10-31 17:39:05 +0100

    character selection picture click selects

--- Assets/Paun/scripts/characterChoosing.cs
+++ Assets/Paun/scripts/characterChoosing.cs
@@ -9,6 +9,22 @@ public class characterChoosing : MonoBehaviour
     public Toggle female;
     public Toggle male;
     public GameObject go;
+
+    public void pictureGirlClick()
+    {
+        female.isOn = !female.isOn;
+        male.isOn = false;
+        //male.enabled = false;
+        go.GetComponent<Text>().text = "girl";
+    }
+
+    public void pictureBoyClick()
+    {
+        male.isOn = !male.isOn;
+        female.isOn = false;
+        go.GetComponent<Text>().text = "boy";
+    }
+
     public void girl(bool checkGirl)
     {
         female.enabled = !checkGirl;
